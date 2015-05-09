25db4b8f8cfeaabcb0d211abc37bced28c3d9afa
stinkymark1 <stinkymark1@gmail.com>
2015-05-10 00:23:07 +0300

    1) изменил гульп файлик 2) подправил пути к либам

--- gulp_dima/gulpfile.js
+++ gulp_dima/gulpfile.js
@@ -2,33 +2,19 @@
 var gulp = require('gulp'); 
 
 // Include Our Plugins
-//var jshint = require('gulp-jshint');
 var less = require('gulp-less');
-//var concat = require('gulp-concat');
-//var uglify = require('gulp-uglify');
-//var rename = require('gulp-rename');
 
-var FolderCss = "../assets/css";
-
-// Compile Our bootstrap less
-gulp.task('bootstrap', function() {
-    return gulp.src('../src/less/bootstrap/bootstrap.less')
-        .pipe(less())
-        .pipe(gulp.dest(FolderCss));
-});
-
-// Compile Our bootstrap less
+// Compile Our less
 gulp.task('less', function() {
-    return gulp.src('../src/less/placeview.less')
+    return gulp.src('../less/placeview.less')
         .pipe(less())
-        .pipe(gulp.dest(FolderCss));
+        .pipe(gulp.dest('../web/css'));
 });
-
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('../src/less/placeview.less', ['less']);
+    gulp.watch('../less/placeview.less', ['less']);
 });
 
 // Default Task
-gulp.task('default', ['less', 'bootstrap-less', 'watch']);
+gulp.task('default', ['lint', 'less', 'scripts', 'watch']);
