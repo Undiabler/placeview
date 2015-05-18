edfe7d8e7afaf0f0f52876a736683b0c0723e2b1
stinkymark1 <stinkymark1@gmail.com>
2015-05-18 16:00:08 +0300

    1) быстрее появляется и прячеться менюшка при клике

--- web/js/placeview.js
+++ web/js/placeview.js
@@ -23,7 +23,7 @@ $(function() {
 	});
 	$(".hover_menu button.hide_show").click(function(){
 		$(this).parent().toggleClass("minified");
-		$(this).parent().children(".menu_body").fadeToggle();
+		$(this).parent().children(".menu_body").fadeToggle("fast");
 	});
 	$(".toombler .elem").click(function(){
 		$(".toombler .elem").each(function(){
