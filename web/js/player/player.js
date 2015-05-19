2ec9e7669e3e3fefb70fa7fab6b6504820c08ae5
stinkymark1 <stinkymark1@gmail.com>
2015-05-19 15:10:52 +0300

    1) поменял размер ленты вотографов и туров

--- web/js/player/player.js
+++ web/js/player/player.js
@@ -1,15 +1,31 @@
 (function(){
+	var show_hide_panos, multimenu, select_lang;
+	show_hide_panos = document.getElementById('show_hide_panos');
+	multimenu = document.getElementById('multimenu');
+	select_lang = multimenu.getElementsByClassName('lang')[0];
 	elem_position();
 	
 	window.onresize = function() {
 		elem_position()
 	}
-	document.getElementById('show_hide_panos').getElementsByClassName('hide')[0].addEventListener('click', function(){
+	show_hide_panos.getElementsByClassName('hide')[0].addEventListener('click', function(){
 		this.parentNode.classList.remove('active');
 	})
-	document.getElementById('show_hide_panos').getElementsByClassName('show')[0].addEventListener('click', function(){
+	show_hide_panos.getElementsByClassName('show')[0].addEventListener('click', function(){
 		this.parentNode.classList.add('active');
 	});
+	select_lang.addEventListener('click', function(){
+		this.classList.toggle('active');
+	});
+	select_lang.getElementsByClassName('list').forEach(function(item){
+	});
+
+
+/*
+	.addEventListener('click',function(){
+		alert(this.innerHTML)
+	});
+*/
 	function elem_position() {
 		document.getElementsByClassName("slides")[0].style.height = window.innerHeight-document.getElementById("top_menu").clientHeight + "px";
 		document.getElementById("multimenu").style.height = window.innerHeight-document.getElementById("top_menu").clientHeight + "px";
