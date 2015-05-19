716f6cbe651d39df965cf3acd81f6fde211b8b6c
stinkymark1 <stinkymark1@gmail.com>
2015-05-19 12:34:22 +0300

    1) внес измениния в правую панель управления, добавил иконки

--- web/js/player/player.js
+++ web/js/player/player.js
@@ -1,9 +1,17 @@
 (function(){
-	document.getElementsByClassName("slides")[0].style.height = window.innerHeight-document.getElementById("top_menu").clientHeight + "px";
+	elem_position();
+	
 	window.onresize = function() {
-		document.getElementsByClassName("slides")[0].style.height = window.innerHeight-document.getElementById("top_menu").clientHeight + "px";
+		elem_position()
 	}
-	document.getElementById("show_hide_panos").addEventListener("click", function(){
-		this.classList.toggle('active')
+	document.getElementById('show_hide_panos').getElementsByClassName('hide')[0].addEventListener('click', function(){
+		this.parentNode.classList.remove('active');
+	})
+	document.getElementById('show_hide_panos').getElementsByClassName('show')[0].addEventListener('click', function(){
+		this.parentNode.classList.add('active');
 	});
+	function elem_position() {
+		document.getElementsByClassName("slides")[0].style.height = window.innerHeight-document.getElementById("top_menu").clientHeight + "px";
+		document.getElementById("multimenu").style.height = window.innerHeight-document.getElementById("top_menu").clientHeight + "px";
+	}
 })();
